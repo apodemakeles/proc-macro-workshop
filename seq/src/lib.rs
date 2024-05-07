a06ef7b6e9648fe1c42101a28bbd6a1649ad08c9
caozheng <caozheng@trolia.com>
2024-05-07 13:31:36 +0800

    01

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,27 @@
-use proc_macro::TokenStream;
+use proc_macro2::Ident;
+use syn::parse::{Parse, ParseStream};
+use syn::{Expr, LitInt, parse_macro_input, Token};
+
+struct Seq{
+}
+
+impl Parse for Seq{
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let _number: Ident = input.parse()?;
+        let _ = input.parse::<Token![in]>()?;
+        let _start: LitInt = input.parse()?;
+        let _ = input.parse::<Token![..]>()?;
+        let _end: LitInt = input.parse()?;
+        let _ = input.parse::<Token![..]>()?;
+        let _: Expr = input.parse()?;
+
+        Ok(Seq{})
+    }
+}
 
 #[proc_macro]
-pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as Seq);
 
-    unimplemented!()
+    proc_macro::TokenStream::new()
 }
