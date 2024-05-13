72eeb172c79af7c17e579a6008c53220e7ad0bb3
caozheng <caozheng@trolia.com>
2024-05-13 21:07:14 +0800

    02

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,21 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{Item, parse_macro_input};
+use syn::spanned::Spanned;
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+pub fn sorted(args: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as Item);
+    let args: TokenStream = args.into();
+    match input{
+        Item::Enum(ref _item) =>{},
+        _=>{
+            return syn::Error::new(args.span(), "expected enum or match expression").into_compile_error().into();
+        }
+    }
+    let tokens = quote! {
+        #input
+    };
 
-    unimplemented!()
+    tokens.into()
 }
