46b43f72f17538f5356063d60a02438baadeaffc
caozheng <caozheng@trolia.com>
2024-05-14 08:44:53 +0800

    03

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -3,19 +3,34 @@ use quote::quote;
 use syn::{Item, parse_macro_input};
 use syn::spanned::Spanned;
 
+fn check(args: TokenStream, input: Item) -> TokenStream{
+    match input{
+        Item::Enum(ref item) =>{
+            let mut names: Vec<String> = vec![];
+            for variant in item.variants.iter() {
+                let cur_name = variant.ident.to_string();
+                for name in names.iter() {
+                    if &cur_name < name{
+                        let msg = format!("{} should sort before {}", cur_name, name);
+                        return syn::Error::new(variant.span(), msg).into_compile_error();
+                    }
+                }
+                names.push(cur_name);
+            }
+            return quote! { #input }
+        },
+        _=>{
+            return syn::Error::new(args.span(), "expected enum or match expression").into_compile_error();
+        }
+    }
+}
+
+
 #[proc_macro_attribute]
 pub fn sorted(args: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as Item);
     let args: TokenStream = args.into();
-    match input{
-        Item::Enum(ref _item) =>{},
-        _=>{
-            return syn::Error::new(args.span(), "expected enum or match expression").into_compile_error().into();
-        }
-    }
-    let tokens = quote! {
-        #input
-    };
-
-    tokens.into()
+    check(args.into(), input).into()
 }
+
+
